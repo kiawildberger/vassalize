ec935029bba75a634ba1d59ada64de9bc192e832
kia <53071526+kiawildberger@users.noreply.github.com>
2020-04-14 21:37:47 -0700

    send msgs to channels without user sending first

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
 const { ipcRenderer } = require("electron")
 function id(e) { return document.getElementById(e) }
 
-let currentchannel;
-
 ipcRenderer.on("msg", (event, arg) => {
     let ul = document.createElement("ul")
     ul.classList.add("msg")
-    ul.textContent = arg.msg.author.username+"#"+arg.msg.author.discriminator+": "+arg.msg.content
+    ul.textContent = arg.msg.author.username + "#" + arg.msg.author.discriminator + ": " + arg.msg.content
     ul.setAttribute("data-channel", arg.msg.channel)
     id("msgdisplay").appendChild(ul)
+    id('msgdisplay').scrollTop = id('msgdisplay').scrollHeight
 })
 
-let logged;
-ipcRenderer.on("log", (event, args) => {
-    logged = args
+let gids, gna = [], ccids = [], cna = [];
+ipcRenderer.on("gids", (event, arg) => {
+    arg.forEach(e => {
+        let opt = document.createElement("option")
+        opt.setAttribute("value", e.name)
+        opt.innerText = e.name
+        id('guilds').appendChild(opt)
+        gna.push(e.name)
+    })
+    gids = arg
+    console.log(gna)
+})
+let currentchannel;
+id('guilds').addEventListener('change', e => {
+    // let r = e.target.innerText.split("\n")
+    // let guild = gids[r.indexOf(e.target.value)] // is guild ig
+    // let guild = gids[gna.indexOf(e.target.value)].id
+    let idx = gna.indexOf(e.target.value)
+    ipcRenderer.send("rcids", idx)
+})
+
+
+ipcRenderer.on("cids", (event, arg) => {
+    id('channels').innerHTML = ''
+    arg.forEach(e => {
+        let opt = document.createElement("option")
+        opt.setAttribute("value", e.name)
+        opt.innerText = e.name
+        id('channels').appendChild(opt)
+        ccids.push(e.id)
+        cna.push(e.name)
+    })
 })
 
 id("msgin").addEventListener("keypress", e => {
-    if(e.keyCode !== 13) return;
+    if (e.keyCode !== 13) return;
+    currentchannel = ccids[cna.indexOf(id('channels').value)]
     ipcRenderer.send("msgin", {
-        msg: id("msgin").value, 
-        channel: id('msgdisplay').children[id('msgdisplay').children.length-1].getAttribute("data-channel")
+        msg: id("msgin").value,
+        channel: currentchannel
     })
     id('msgin').value = ""
 })
--- src/ps.js
+++ src/ps.js
@@ -10,20 +10,35 @@ let settings = {
 let window;
 let lastmessage;
 let isLoggedIn = false
+let gids = []
+let cids = []
 
 exports.init = function(win, tok) {
     window = win;
     const client = new Discord.Client();
     client.on("ready", () => {
         console.log("I am ready!");
+        client.guilds.cache.array().forEach(t => {
+            gids.push(t)
+            isLoggedIn = true
+        })
+        window.webContents.send("gids", gids)
     });
     client.on("message", (message) => {
         process(message)
     });
     client.login(tok);
-    isLoggedIn = true
     ipcMain.on("msgin", (event, arg) => {
         client.channels.cache.get(arg.channel).send(arg.msg)
+    })
+    // channels.cache.filter(c => c.type === "text")
+    ipcMain.on("rcids", (event, guild) => {
+        let cids = []
+        let e = gids[guild];
+        e.channels.cache.filter(c => c.type === "text").array().forEach(r => {
+            cids.push(r)
+        })
+        window.webContents.send("cids", cids)
     })
 }
 
