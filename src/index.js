dc2bc2e4cdfa2af453c31a5798695ecfa47d7d73
kia <53071526+kiawildberger@users.noreply.github.com>
2021-01-26 17:47:58 -0800

    undid the file merge in a bad way

--- src/index.js
+++ src/index.js
@@ -102,12 +102,11 @@ let mdurlregex = /(\[.*\])(.*)/g
 let imagetypes = ["jpg", "JPG", 'png', 'PNG', 'gif', 'GIF', 'webp', 'WEBP', 'tiff', 'TIFF', 'jpeg', 'JPEG', 'svg', 'SVG']
 let vidtypes = ["mp4", "MP4", "webm", "WEBM", "mkv", "MKV", "ogg", "OGG", "ogv", "OGV", "avi", "AVI", "gifv", "GIFV", "mpeg", "MPEG"]
 let gids = [], index = 0, currentchannel;
-eventlistener.addEventListener("msg", e => {
-  processmsg(arg.data);
+ipcRenderer.on("msg", (e, arg) => {
+  processmsg(arg);
 })
 
-function processmsg(g) {
-  let arg = {msg:g}
+function processmsg(arg) {
   let e, link
   let hasmdlink = arg.msg.content.match(mdurlregex)
   if (hasmdlink) {
@@ -299,7 +298,7 @@ function fillGuildSelect(arg) { // generates and populates guild list
           })
           elm.classList.add("activechannel")
           id('msgdisplay').innerHTML = ''
-          getChannelContent(e.id)
+          ipcRenderer.send("getChannelContent", e.id)
         })
         id("channellist").appendChild(elm)
         if (server.channels.indexOf(e) === 0) elm.dispatchEvent(new Event('click'))
@@ -323,13 +322,13 @@ function fillGuildSelect(arg) { // generates and populates guild list
   id('loginbg').style.display = 'none'
 }
 
-function invalidtoken() {
+ipcRenderer.on("invalidtoken", () => {
   id('bs-helper').style.color = 'red'
   id('bs-helper').textContent = "something went wrong and could not login"
   setTimeout(() => {
     id('bs-helper').textContent = ''
   }, 3000)
-}
+})
 
 async function checkCached() {
   if (conf) {
@@ -341,7 +340,7 @@ async function checkCached() {
       r.addEventListener("click", async () => {
         r.setAttribute('state', "pending")
         setTimeout(() => r.setAttribute("state", "selected"), 100)
-        init(conf[i])
+        ipcRenderer.send("startbot", conf[i])
       })
       id('cached').appendChild(r)
     }
@@ -385,8 +384,8 @@ function fillStatus(bot, presenceData = undefined) {
 }
 ipcRenderer.on("statusUpdate", (event, arg) => fillStatus(botActor, arg.presenceData));
 
-// ipcRenderer.on("validtoken", (event, args) => { // the *one* ipc i will use
-function validtoken(bot, guildInfo) {
+ipcRenderer.on("validtoken", (event, args) => { // the *one* ipc i will use
+  let { bot, guildInfo } = args;
   if (bot && guildInfo) {
     botActor = bot, gids = guildInfo
     document.querySelector(".serverlist-label").innerHTML += `<span> (${gids.length})</span>`;
@@ -402,11 +401,11 @@ function validtoken(bot, guildInfo) {
     id('bs-helper').innerText = ''
     fillGuildSelect(guildInfo)
   }, 800)
-}
+})
 id("bot-secret").addEventListener("keypress", e => {
   if (e.code != "Enter") return; // keyCode is apparently deprecated?
   if (!e.target.value) return;
-  init(e.target.value)
+  ipcRenderer.send("startbot", e.target.value)
 })
 id("bot-secret").addEventListener("keydown", (e) => {
   if(e.key === "Backspace" && e.target.value.length <= 1) {
@@ -430,19 +429,19 @@ setInterval(() => {
 }, 1500)
 id("msgin").addEventListener("keypress", e => {
   if (!istyping) {
-    typing(true, currentchannel)
+    ipcRenderer.send("typing", {chid:currentchannel, stop:true}) // is this the right one?
     istyping = true
   }
   if (id("msgin").value === "") {
-    typing(false, currentchannel)
+    ipcRenderer.send("typing", {chid:currentchannel, stop:true})
     istyping = false
   }
   if (e.keyCode !== 13) return;
   if (!e.target.value) return;
   id('bs-helper').innerText = ''
-  typing(false, currentchannel)
+  ipcRenderer.send("typing", {chid:currentchannel,stop:true})
   istyping = false;
-  msgin(id('msgin').value, currentchannel)
+  ipcRenderer.send("msgin", {msg: id('msgin').value, channel: currentchannel})
   id('msgin').value = ""
 })
 if(Object.keys(require("./config.json")).length > 0) {
@@ -565,282 +564,3 @@ function messageUpdate(id, content) {
 id("custom-script-input").addEventListener("change", e => {
   scriptreader.processFiles(e)
 })
-
-//                  this was previously ps.js
-// -----------------------------------------------------------------
-
-/**
- * 
- * @param {Boolean} ss true to start, false to stop
- * @param {Number} chid channel id to be typing in
- */
-function typing(ss, chid) {
-  if (!Rsettings.typing) return;
-  if (ss) {
-    client.channels.cache.get(chid).startTyping()
-  } else if (!ss) {
-    client.channels.cache.get(chid).stopTyping()
-  }
-}
-
-let uinfo = {}
-let window;
-let lastmessage;
-let isLoggedIn = false
-// let gids = []
-let cids = [];
-let uidx = /<@!\d+>/g;
-let client;
-let scripts = require("./scripts.json")
-let enabledscripts = scripts.filter(e => e.enabled)
-
-function logFile(e) {
-  if (Rsettings.fileLogging) fs.appendFile("./logfile", e + "\n", () => { })
-}
-
-function getChannelContent(id) {
-  let b = client.channels.cache.get(id)
-  if (b) {
-    b.messages.fetch().then((resp) => {
-      let t = resp.array()
-      t.length = Rsettings.cachedlength
-      t = t.reverse()
-      t.forEach(msg => {
-        let ct = msg.content
-        if (msg.mentions) {
-          let mts = msg.mentions.users.array()
-          mts.forEach(e => {
-            let i = `@${e.username}#${e.discriminator}`
-            ct = ct.replace(uidx, i)
-          })
-        }
-        let m = {
-          content: ct,
-          author: {
-            username: msg.author.username,
-            discriminator: msg.author.discriminator,
-            id: msg.author.id,
-            avatar: msg.author.avatarURL()
-          },
-          channel: msg.channel.id
-        }
-        if (msg.attachments) {
-          let d = msg.attachments.array()
-          d.forEach(e => {
-            e = e.url
-          })
-          m.images = d
-        }
-        setTimeout(() => {
-          process(msg, true)
-        }, 100)
-      })
-    }).catch(err => {
-      if (err.name === "DiscordAPIError" && err.message === "Missing Access") {
-        return {
-          err: "missing access"
-        }
-      }
-    })
-  } else { // could not fetch messages for some unknown reason
-    return {
-      err: "could not fetch messages ¯\\_(ツ)_/¯"
-    }
-  }
-}
-
-function process(msg, iscached = false) {
-  clearmodule("./settings.json");
-  Rsettings = require("./settings.json")
-  if (Rsettings.csenabled && !iscached) {
-    // window.webContents.send("refreshScript")
-    scriptreader.update() // is the event above supposed to do this? was missing an s idk
-    enabledscripts.forEach(e => {
-      try {
-        clearmodule(e.path)
-        let script = require(e.path)
-        var g = script.message(msg)
-        if (g) logFile(e.name + " > " + g)
-      } catch(err) {
-        logFile("[Script] " + e.name + " was unable to receive a message")
-        console.log(`[Script] ${e.name} was unable to receive a message`)
-        throw err;
-      }
-    })
-  }
-  let ct = msg.content
-  if (msg.mentions.users.array().length >= 1) {
-    let mts = msg.mentions.users.array()
-    mts.forEach(e => {
-      let i = `@${e.username}#${e.discriminator}`
-      ct = ct.replace(`<@${e.id}>`, i)
-      ct = ct.replace(`<@!${e.id}>`, i)
-    })
-  }
-  let avatarURL = msg.author.displayAvatarURL()
-  // if(avatarURL === null) {
-  //   avatarURL = msg.author.defaultAvatarURL()
-  // }
-  let m = {
-    content: ct,
-    id: msg.id,
-    author: {
-      username: msg.author.username,
-      discriminator: msg.author.discriminator,
-      id: msg.author.id,
-      avatar: avatarURL
-    },
-    channel: msg.channel.id,
-    guild: msg.guild.id, // what
-    time: msg.createdTimestamp
-  }
-  if (msg.mentions.users.array().length != 0) {
-    m.mentions = msg.mentions.users.array()
-  }
-  if (msg.attachments.size) {
-    m.images = msg.attachments.array()
-  }
-  // window.webContents.send("msg", {
-  //   msg: m
-  // })
-  processmsg(m)
-  if (msg.content.includes("discord.gg")) {
-    console.log('someone sent a discord invite link: ' + msg.content)
-  }
-}
-
-function msgin(msg, channel) {
-  let arg = {msg, channel}; // so i dont have to replace all instances :grimace:
-  let tagx = /@.*#[0-9]{4}/;
-  if (arg.msg.toString().match(tagx)) { // is ping
-    let un = arg.msg.toString().match(tagx)[0].toString().split("#")[0].replace("@", '')
-    let g = client.users.cache.find(x => x.username === un)
-    console.log(un, g)
-    arg.msg = arg.msg.replace(`@${g.username}#${g.discriminator}`, g)
-  }
-  if (arg.msg.toString().includes("/shrug")) arg.msg = arg.msg.replace(/\/shrug/g, "¯\_(ツ)_/¯")
-  client.channels.cache.get(arg.channel).send(arg.msg)
-}
-
-function init(tok) {
-  client = new Discord.Client()
-  client.on("ready", () => {
-    if (isLoggedIn) return;
-    if(Rsettings.status) client.user.setPresence(Rsettings.status)
-    clearmodule("./settings.json");
-    Rsettings = require("./settings.json")
-    if (enabledscripts.length > 0 && Rsettings.csenabled) {
-      enabledscripts.forEach(e => {
-        try {
-          clearmodule(e.path)
-          let script = require(e.path)
-          if (script.init) var g = script.init(client)
-          logFile("[Script] " + e.name + " started successfully")
-          console.log(`[Script] ${e.name} started successfully`)
-          if (g) {
-            logFile('[Script]'+e.name + " > " + g)
-          }
-        } catch (err) {
-          logFile("[Script] " + e.name + " failed to start")
-          throw err;
-        }
-      })
-    }
-    client.guilds.cache.array().forEach(t => {
-      let channels = [],
-        vchannels = [],
-        msgcontent = [],
-        emojis = [],
-        emojids = [];
-      t.channels.cache.array().forEach(e => {
-        if (e.type === "text" || e.type === "news") {
-          channels.push({
-            name: e.name,
-            id: e.id
-          })
-        } else if(e.type === "voice") {
-          vchannels.push({
-            name: e.name,
-            id: e.id
-          })
-        }
-      })
-      t.emojis.cache.array().forEach(e => {
-        emojis.push({
-          name: e.name,
-          id: e.id,
-          url: e.url
-        })
-        emojids.push(e.id)
-      })
-      let ob = {
-        name: t.name,
-        id: t.id,
-        channels: channels,
-        emoji: emojis,
-        emojiIds: emojids
-      }
-      if (t.icon) ob.icon = `https://cdn.discordapp.com/icons/${t.id}/${t.icon}.png`
-      if (!t.icon) ob.abbr = t.nameAcronym
-      gids.push(ob)
-      isLoggedIn = true
-    })
-    let tokens = {}
-    let botuser = client.user.username + "#" + client.user.discriminator
-    uinfo = {
-      name: client.user.username,
-      discrim: client.user.discriminator,
-      full: botuser,
-      pfp: client.user.avatarURL(),
-      guildNumber: client.guilds.cache.array().length,
-      id: client.user.id
-    }
-    clearmodule("./config.json")
-    clearmodule("./settings.json")
-    Rsettings = require("./settings.json")
-    tokens = require("./config.json")
-    tokens[botuser] = tok
-    if(Rsettings.cachetokens) fs.writeFileSync("./config.json", JSON.stringify(tokens));
-    // window.webContents.send("validtoken", {
-    //   bot: uinfo,
-    //   guildInfo: gids
-    // })
-    validtoken(uinfo, gids)
-  });
-  client.on("message", (message) => {
-    process(message)
-  });
-  client.on("messageDelete", message => {
-    // window.webContents.send("messagedeleted", message.id)
-    messageDeleted(message.id);
-  });
-  client.on("messageUpdate", (oldMessage, newMessage) => {
-    // window.webContents.send("messageupdated", {id: oldMessage.id, content: newMessage.content})
-    messageUpdate(oldMessage.id, newMessage.content)
-  })
-  client.login(tok)
-    .catch(err => {
-      // window.webContents.send('invalidtoken')
-      console.log(tok)
-      invalidtoken()
-    })
-
-  // both of these status events are from status.html so they get to stay
-  ipcRenderer.on("clearstatus", () => {
-    client.user.setActivity(null);
-    document.querySelector(".status-pres").innerHTML = 'Set a status'
-    clearmodule("./settings.json")
-    let newsettings = require("./settings.json")
-    if(newsettings.status) delete newsettings.status
-    fs.writeFileSync("./settings.json", JSON.stringify(newsettings));
-  });
-  ipcRenderer.on("statusentered", (event, arg) => {
-    // pain
-    clearmodule("./settings.json")
-    let newsettings = require("./settings.json")
-    newsettings.status = arg.presenceData
-    fs.writeFileSync("./settings.json", JSON.stringify(newsettings))
-    client.user.setPresence(arg.presenceData)
-    win.webContents.send("statusUpdate", {presenceData: arg.presenceData})
-  })
-}
